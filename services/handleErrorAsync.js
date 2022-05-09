43343dd151fe6394fa3ddf3e14999d311fe838c6
konstante <konstantechang@gmail.com>
2022-05-09 19:57:10 +0800

    add services directory

---
+++ services/handleErrorAsync.js
@@ -0,0 +1,12 @@
+const handleErrorAsync = function handleErrorAsync(func){
+    return function(req, res, next){
+        func(req, res, next).catch(
+            function(error){
+                return next(error);
+            }
+        );
+    };
+};
+
+
+module.exports = handleErrorAsync;

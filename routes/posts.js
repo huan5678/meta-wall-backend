6a2c01ea6cc745d17016bbec58870ef01e27fff0
nimo <>
2022-05-20 22:21:33 +0800

    [add] posts/get.router

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-
-
+const Post = require("../models/post");
+//資料全撈
+router.get("/", async (req, res) => {
+  res.status(200).json({
+    status: "200",
+    post: await Post.find()
+  });
+});
 module.exports = router;

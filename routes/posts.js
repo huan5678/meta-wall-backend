98e77040f248fd4cb0426208ba8ddb8700606955
nimo <>
2022-05-21 20:51:56 +0800

    [add] 加入successHandle

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Post = require("../models/post");
+const Post = require('../models/post');
+const successHandle = require('../utils/successHandle');
 //資料全撈
-router.get("/", async (req, res) => {
-  res.status(200).json({
-    status: "200",
-    post: await Post.find()
-  });
+router.get('/', async (req, res) => {
+  const post = await Post.find();
+  successHandle(res, '全部貼文', post);
 });
 module.exports = router;

bd135e39dfc95fde678a1a354854f6f8c42c4a87
nimo <>
2022-05-24 23:29:12 +0800

    [add] get 單一貼文

--- routes/posts.js
+++ routes/posts.js
@@ -10,8 +10,8 @@ router.get('/', async (req, res) => {
   const post = await Post.find();
   successHandle(res, '成功撈取所有貼文', post);
 });
-
-router.post('/', isAuthor, postController.postCreate);
+router.get('/:id', postController.getOne);
+router.post('/create', isAuthor, postController.postCreate);
 router.delete('/:id', isAuthor, postController.postDelete);
 router.post('/:id/likes', isAuthor, postController.addLike);
 router.delete('/:id/likes', isAuthor, postController.deleteLike);

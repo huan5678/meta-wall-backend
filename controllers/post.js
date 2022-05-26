d3fe3bb4b1c8dedabf4ec62ac107c9f8fbea778f
nimo <>
2022-05-26 22:43:54 +0800

    [add] 增加getAll跟postPatch

--- controllers/post.js
+++ controllers/post.js
@@ -4,12 +4,16 @@ const successHandle = require('../utils/successHandle');
 const appError = require('../utils/appError');
 
 const postController = {
+  getAll: handleErrorAsync(async (req, res, next) => {
+    const post = await Post.find();
+    successHandle(res, '成功撈取所有貼文', post);
+  }),
   getOne: handleErrorAsync(async (req, res, next) => {
-    const _id = req.params.id;
+    const { id: _id } = req.params;
     if (!_id) {
       return next(appError(400, '無此貼文', next));
     }
-    let getOneResult = await Post.findById({ _id });
+    let getOneResult = await Post.findById({ _id }, { _id: 0 });
     return successHandle(res, '成功取得一則貼文', getOneResult);
   }),
   postCreate: handleErrorAsync(async (req, res, next) => {
@@ -54,6 +58,16 @@ const postController = {
     await Post.findOneAndUpdate({ _id }, { $pull: { likes: req.user.id } });
     successHandle(res, '刪除一個讚', { postId: _id, useId: req.user.id });
   }),
+  postPatch: handleErrorAsync(async (req, res, next) => {
+    const { body } = req;
+    const { id } = req.params;
+    const editPost = await Post.findByIdAndUpdate(id, body);
+    if (body.content && editPost) {
+      console.log(editPost);
+      const editda = await Post.findById(editPost._id);
+      successHandle(res, '成功編輯一則貼文!!', editda);
+    }
+    return next(appError(400, '請檢查content 資料', next));
+  }),
 };
-
 module.exports = postController;

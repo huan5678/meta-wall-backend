d07b019e01f58cdde928b0a61cb6be8501cd8ba6
nimo <>
2022-05-26 22:42:38 +0800

    [add] 增加getAll與postPatch

--- routes/posts.js
+++ routes/posts.js
@@ -6,14 +6,11 @@ const postController = require('../controllers/post');
 const { isAuthor } = require('../middleware/handleJWT');
 
 //資料全撈
-router.get('/', async (req, res) => {
-  const post = await Post.find();
-  successHandle(res, '成功撈取所有貼文', post);
-});
+router.get('/', postController.getAll);
 router.get('/:id', postController.getOne);
 router.post('/create', isAuthor, postController.postCreate);
 router.delete('/:id', isAuthor, postController.postDelete);
 router.post('/:id/likes', isAuthor, postController.addLike);
 router.delete('/:id/likes', isAuthor, postController.deleteLike);
-
+router.patch('/:id', isAuthor, postController.postPatch);
 module.exports = router;

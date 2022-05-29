7e88188c41bc8cef904c86ab16a80d863e4e98c7
konstante <konstantechang@gmail.com>
2022-05-29 23:33:20 +0800

    add change 因rebase失敗, 重新git clone 重開分支 init

--- routes/posts.js
+++ routes/posts.js
@@ -17,3 +17,236 @@ router.post('/:id/comment', isAuthor, postController.addComment);
 router.get('/user/:id', postController.getUserPosts);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /posts:
+ *   get:
+ *     tags:
+ *       - Post 貼文
+ *     summary: 取得所有貼文
+ *     security:
+ *       - bearerAuth: []
+ *     description: 取得所有貼文
+ *     responses:
+ *       200:
+ *         description: 取得所有貼文
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: Boolean
+ *                   description: status
+ *                   example: true
+ *                 message:
+ *                   type: String
+ *                   description: message
+ *                   example: 成功撈取所有貼文
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         example: 628ece82750413a09f24eb29
+ *                       content:
+ *                         type: string
+ *                         example: I am Malenia
+ *                       image:
+ *                         type: string
+ *                         example: eldenring.io
+ *                       userId:
+ *                         type: object
+ *                         properties:
+ *                           _id:
+ *                             type: string
+ *                             example: 628e4f69cc07c6ccd79f783b
+ *                           name:
+ *                             type: string
+ *                             example: malenia
+ *                           photo:
+ *                             type: string
+ *                             example: eldenring.io
+ *                       likes:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                           example: 628e4f69cc07c6ccd79f783b
+ */
+
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /posts/{id}:
+ *   get:
+ *     tags:
+ *       - Post 貼文
+ *     summary: 取得單一貼文
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: 要取得的貼文id 
+ *     security:
+ *       - bearerAuth: []
+ *     description: 取得單一貼文
+ *     responses:
+ *       200:
+ *         description: 取得單一貼文
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: Boolean
+ *                   description: status
+ *                   example: true
+ *                 message:
+ *                   type: String
+ *                   description: message
+ *                   example: 成功取得一則貼文
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     content:
+ *                       type: string
+ *                       example: I am Malenia
+ *                     image:
+ *                       type: string
+ *                       example: eldenring.io
+ *                     userId:
+ *                       type: object
+ *                       properties:
+ *                         _id:
+ *                           type: string
+ *                           example: 628e4f69cc07c6ccd79f783b
+ *                         name:
+ *                           type: string
+ *                           example: malenia
+ *                         photo:
+ *                           type: string
+ *                           example: eldenring.io
+ *                     likes:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                         example: 628e4f69cc07c6ccd79f783b
+ */
+
+
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /posts/create:
+ *   post:
+ *     tags:
+ *       - Post 貼文
+ *     summary: 新增一則貼文
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               content:
+ *                 type: string
+ *                 example: I am Malenia 
+ *     security:
+ *       - bearerAuth: []
+ *     description: 新增一則貼文
+ *     responses:
+ *       200:
+ *         description: 成功新增一則貼文
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: Boolean
+ *                   description: status
+ *                   example: true
+ *                 message:
+ *                   type: String
+ *                   description: message
+ *                   example: 成功新增一則貼文
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       example: 628e4f69cc07c6ccd79f783b
+ *                     content:
+ *                       type: string
+ *                       example: I am Malenia
+ *                     image:
+ *                       type: string
+ *                       example: eldenring.io
+ */
+
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /posts/{id}:
+ *   delete:
+ *     tags:
+ *       - Post 貼文
+ *     summary: 刪除貼文
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: 要刪除的貼文id 
+ *     security:
+ *       - bearerAuth: []
+ *     description: 刪除貼文
+ *     responses:
+ *       200:
+ *         description: 刪除一則貼文
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: status
+ *                   example: success
+ *                 message:
+ *                   type: String
+ *                   description: message
+ *                   example: 刪除一則貼文
+ */

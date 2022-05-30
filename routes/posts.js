f0b0c248e91be051e775cd4dcc75504207097fc8
konstante <konstantechang@gmail.com>
2022-05-30 18:56:30 +0800

    [modified]新增routes/posts.js 的swagger註解

--- routes/posts.js
+++ routes/posts.js
@@ -248,3 +248,160 @@ module.exports = router;
  *                   description: message
  *                   example: 刪除一則貼文
  */
+
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /posts/{id}:
+ *   patch:
+ *     tags:
+ *       - Post 貼文
+ *     summary: 修改貼文
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: 要修改的貼文id 
+ *     security:
+ *       - bearerAuth: []
+ *     description: 修改貼文
+ *     responses:
+ *       200:
+ *         description: 修改一則貼文
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: Boolean
+ *                   description: status
+ *                   example: true
+ *                 message:
+ *                   type: String
+ *                   description: message
+ *                   example: 成功編輯一則貼文!!
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       example: 628ed7e88a070399e72fd824
+ *                     content:
+ *                       type: string
+ *                       example: 修改貼文
+ *                     image:
+ *                       type: string
+ *                       example: eldenring.io
+ *                     userId:
+ *                       type: object
+ *                       properties:
+ *                         _id:
+ *                           type: string
+ *                           example: 628e4f69cc07c6ccd79f783b
+ *                         name:
+ *                           type: string
+ *                           example: malenia
+ *                         photo:
+ *                           type: string
+ *                           example: eldenring.io
+ *                     likes:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                         example: 628e4f69cc07c6ccd79f783b
+ *                     createdAt:
+ *                       type: string
+ *                       example: 2022-05-26T01:29:12.645Z
+ */
+
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /posts/{id}/likes:
+ *   post:
+ *     tags:
+ *       - Post 貼文
+ *     summary: 貼文按讚
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: 要按讚的貼文id 
+ *     security:
+ *       - bearerAuth: []
+ *     description: 按讚貼文
+ *     responses:
+ *       200:
+ *         description: 按讚一則貼文
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: Boolean
+ *                   description: status
+ *                   example: true
+ *                 message:
+ *                   type: String
+ *                   description: message
+ *                   example: 新增一個讚
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /posts/{id}/likes:
+ *   delete:
+ *     tags:
+ *       - Post 貼文
+ *     summary: 貼文取消按讚
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: 要取消按讚的貼文id 
+ *     security:
+ *       - bearerAuth: []
+ *     description: 取消按讚貼文
+ *     responses:
+ *       200:
+ *         description: 取消按讚一則貼文
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: Boolean
+ *                   description: status
+ *                   example: true
+ *                 message:
+ *                   type: String
+ *                   description: message
+ *                   example: 刪除一個讚
+ */
